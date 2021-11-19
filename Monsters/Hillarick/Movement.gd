799db90528834c381bae4fda424269139b42bf91
Rck Hewitt <Rick.Lira.Morgan@gmail.com>
2021-11-18 19:00:17 -0500

    Fixed y direction

--- Monsters/Hillarick/Movement.gd
+++ Monsters/Hillarick/Movement.gd
@@ -17,9 +17,9 @@ func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_down"):
 		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
 
 	# Update sprite direction
 	if velocity.x != 0:
